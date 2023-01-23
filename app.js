1ee0c0ea54cd4789fba15b00be6ea71b6eb1e199
JDevs10 <j-ldsxm18@hotmail.com>
2023-01-24 00:29:13 +0100

    FEAT TIME 0:25 generate map boundries

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
 const Game = {
     READY: false,
+    /** @type {HTMLCanvasElement} */
     gameCanvas: null,
+    /** @type {CanvasRenderingContext2D} */
     gameCanvasContext: null,
+    map: null,
     init: function () {
         this.gameCanvas = document.getElementById('TheGame')
         this.gameCanvasContext = this.gameCanvas.getContext('2d')
         this.gameCanvas.width = window.innerWidth
         this.gameCanvas.height = window.innerHeight
-        App.READY = true
+
+        this.initBoundaries();
+
+        this.READY = true
+    },
+    Boundary: class {
+        static width =  40;
+        static height =  40;
+
+        constructor({ position }) {
+            this.position = position
+            this.width = 40
+            this.height = 40
+        }
+
+        draw() {
+            Game.gameCanvasContext.fillStyle = 'blue'
+            Game.gameCanvasContext.fillRect(this.position.x, this.position.y, this.width, this.height)
+        }
+    },
+    initBoundaries: function () {
+        Game.map = [
+            ['-', '-', '-', '-', '-', '-'],
+            ['-', ' ', ' ', ' ', ' ', '-'],
+            ['-', ' ', '-', '-', ' ', '-'],
+            ['-', ' ', ' ', ' ', ' ', '-'],
+            ['-', '-', '-', '-', '-', '-']
+        ]
+
+        const boundaries = []
+
+        Game.map.forEach((row, i) => {
+            row.forEach((symbol, j) => {
+                switch (symbol) {
+                    case '-':
+                        boundaries.push(
+                            new Game.Boundary({
+                                position: {
+                                    x: Game.Boundary.width * j,
+                                    y: Game.Boundary.height * i
+                                }
+                            })
+                        )
+                        break
+                }
+            })
+        })
+
+        boundaries.forEach((boundary) => {boundary.draw()})
     }
 }
 
