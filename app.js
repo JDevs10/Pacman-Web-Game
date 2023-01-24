0270147b9cfd1904b0bd21b4f018058cfdf3163c
JDevs10 <j-ldsxm18@hotmail.com>
2023-01-24 01:18:06 +0100

    FEAT TIME 0:45 add Pacman with mouvement

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const Game = {
     /** @type {CanvasRenderingContext2D} */
     gameCanvasContext: null,
     map: null,
+    boundaries: [],
+    player: null,
+    keys: {
+        z: {pressed: false},
+        q: {pressed: false},
+        s: {pressed: false},
+        d: {pressed: false}
+    },
+    lastKey: '',
     init: function () {
         this.gameCanvas = document.getElementById('TheGame')
         this.gameCanvasContext = this.gameCanvas.getContext('2d')
         this.gameCanvas.width = window.innerWidth
         this.gameCanvas.height = window.innerHeight
 
-        this.initBoundaries();
+        this.initBoundaries()
+        this.initPlayer()
+        this.initMovementKeys()
+        this.animate()
 
         this.READY = true
     },
@@ -39,13 +51,11 @@ const Game = {
             ['-', '-', '-', '-', '-', '-']
         ]
 
-        const boundaries = []
-
         Game.map.forEach((row, i) => {
             row.forEach((symbol, j) => {
                 switch (symbol) {
                     case '-':
-                        boundaries.push(
+                        Game.boundaries.push(
                             new Game.Boundary({
                                 position: {
                                     x: Game.Boundary.width * j,
@@ -57,8 +67,97 @@ const Game = {
                 }
             })
         })
+    },
+    Player: class {
+        constructor({position, velocity}) {
+            this.position = position
+            this.velocity = velocity
+            this.radius = 15
+        }
 
-        boundaries.forEach((boundary) => {boundary.draw()})
+        draw() {
+            Game.gameCanvasContext.beginPath()
+            Game.gameCanvasContext.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+            Game.gameCanvasContext.fillStyle = 'yellow'
+            Game.gameCanvasContext.fill()
+            Game.gameCanvasContext.closePath()
+        }
+
+        update() {
+            this.draw()
+            this.position.x += this.velocity.x
+            this.position.y += this.velocity.y
+        }
+    },
+    initPlayer: function () {
+        Game.player = new this.Player({
+            position: {
+                x: Game.Boundary.width + Game.Boundary.width / 2,
+                y: Game.Boundary.height + Game.Boundary.height / 2
+            },
+            velocity: {
+                x: 0,
+                y: 0
+            }
+        })
+    },
+    initMovementKeys: function () {
+        addEventListener('keydown', ({key}) => {
+            switch (key) {
+                case 'z':
+                    Game.keys.z.pressed = true
+                    Game.lastKey = 'z'
+                break
+                case 'q':
+                    Game.keys.q.pressed = true
+                    Game.lastKey = 'q'
+                break
+                case 's':
+                    Game.keys.s.pressed = true
+                    Game.lastKey = 's'
+                break
+                case 'd':
+                    Game.keys.d.pressed = true
+                    Game.lastKey = 'd'
+                break
+            }
+        })
+
+        addEventListener('keyup', ({key}) => {
+            switch (key) {
+                case 'z':
+                    Game.keys.z.pressed = false
+                break
+                case 'q':
+                    Game.keys.q.pressed = false
+                break
+                case 's':
+                    Game.keys.s.pressed = false
+                break
+                case 'd':
+                    Game.keys.d.pressed = false
+                break
+            }
+        })
+    },
+    animate: function () {
+        requestAnimationFrame(Game.animate)
+        Game.gameCanvasContext.clearRect(0, 0, Game.gameCanvas.width, Game.gameCanvas.height)
+        Game.boundaries.forEach((boundary) => {boundary.draw()})
+        Game.player.update();
+
+        Game.player.velocity.x = 0
+        Game.player.velocity.y = 0
+
+        if (Game.keys.z.pressed && Game.lastKey === 'z') {
+            Game.player.velocity.y = -5
+        } else if (Game.keys.q.pressed && Game.lastKey === 'q') {
+            Game.player.velocity.x = -5
+        } else if (Game.keys.s.pressed && Game.lastKey === 's') {
+            Game.player.velocity.y = 5
+        } else if (Game.keys.d.pressed && Game.lastKey === 'd') {
+            Game.player.velocity.x = 5
+        }
     }
 }
 
